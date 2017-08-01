8eb54cdc6a6f90efb66d1e245e42d5b3b35d2b9f
James Haver <mchaver@gmail.com>
2017-08-01 23:54:38 +0800

    Authorized request working

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -7,7 +7,7 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Network.HTTP.Affjax (AJAX, Affjax, AffjaxRequest, URL, defaultRequest, affjax, post)
+import Network.HTTP.Affjax (AJAX, Affjax, AffjaxRequest, URL, defaultRequest, affjax)
 
 
 import Data.Either (Either(..), either)
@@ -75,8 +75,15 @@ mkAuthRequest :: forall eff. Eff (cookie :: COOKIE | eff) (Maybe (AffjaxRequest 
 mkAuthRequest = do
   mCook <- liftEff $ getCookie "XSRF-TOKEN"
   case mCook of
-   Nothing -> pure Nothing
-   Just cook -> pure $ Just $ (defaultRequest {headers = [RequestHeader "X-XSRF-TOKEN" cook]})
+   Nothing   -> pure Nothing
+   Just cook -> 
+     pure $ 
+       Just $ 
+         defaultRequest { headers = 
+                           [ RequestHeader "X-XSRF-TOKEN" cook
+                           , Accept (MediaType "*/*")
+                           , ContentType (MediaType "application/json")
+                           ]}
 
 authorizedRequest :: forall eff. Method -> URL -> Eff (console :: CONSOLE, cookie :: COOKIE, ajax :: AJAX, exception :: EXCEPTION | eff) (Unit)
 authorizedRequest method url = void $ launchAff do
@@ -94,16 +101,37 @@ authorizedPost :: forall eff. URL -> Eff (console :: CONSOLE, cookie :: COOKIE, 
 authorizedPost url = authorizedRequest POST url
 
 authPost :: forall e b. Respondable b => URL -> String -> Affjax e b
-authPost url content = affjax $ defaultRequest { method = Left POST, url = url, content = Just ( content)}
+authPost url content = affjax $ defaultRequest { method = Left POST, url = url, content = Just content}
 
-ppPost :: forall e b. Respondable b => URL -> String -> Affjax e b
-ppPost url content = affjax $ defaultRequest { method = Left POST, url = url, content = Just ( content), headers = [Accept (MediaType "*/*"), ContentType (MediaType "application/json")]}
+pGet :: forall e b. Respondable b => (AffjaxRequest Unit) -> URL -> Affjax e b
+pGet request url = affjax $ request { method = Left GET, url = url}
 
+post :: forall e b. Respondable b => URL -> String -> Affjax e b
+post url content = 
+  affjax $ 
+    defaultRequest { method = Left POST
+                   , url = url
+                   , content = Just content
+                   , headers = 
+                     [ Accept (MediaType "*/*")
+                     , ContentType (MediaType "application/json")
+                     ]
+                   }
+
+
+{-
+    RollDieRequest ->
+      let
+        request = HttpB.get "/die/roll" |> HttpB.withExpect (Http.expectJson Json.Decode.int)
+      in
+        (model, sendWithCsrfToken RollDieResponse request)
+-}
 
 data Event = Echo
            | SignIn
            | UsernameChange DOMEvent
            | PasswordChange DOMEvent
+           | RollDie
            
 type State = 
   { username :: String
@@ -111,7 +139,7 @@ type State =
   }
 
 -- | Return a new state (and effects) from each event
-foldp :: Event -> State -> EffModel State Event (ajax :: AJAX, console :: CONSOLE)
+foldp :: Event -> State -> EffModel State Event (ajax :: AJAX, console :: CONSOLE, cookie :: COOKIE)
 foldp Echo state = 
   { state: state
     , effects: [ do 
@@ -119,11 +147,26 @@ foldp Echo state =
       pure Nothing
     ]
   }
+foldp RollDie st = 
+  { state : st
+  , effects : [ do 
+      mReq <- liftEff mkAuthRequest
+      case mReq of
+        Nothing -> do 
+          liftEff $ log "cookie doesn't exist"
+        Just req -> do             
+          res <- attempt $ pGet req "/die/roll"
+          let decode r = decodeJson r.response :: Either String Int
+          let todos = either (Left <<< show) decode res
+          liftEff $ log $ show todos
+      pure Nothing
+  ]}  
+
 foldp SignIn st = 
   { state : st
     , effects : [do
       let login = (show $ encodeJson $ Login {username: st.username, password: st.password})
-      res <- attempt (ppPost "/login" login)
+      res <- attempt (post "/login" login)
       --res <- attempt (post "/login" (show $ encodeJson $ Login {username: st.username, password: st.password}))
       -- res <- attempt (affjax $ defaultRequest { method = Left POST, url = "/login", content = Just login })
       let decode r = decodeJson r.response :: Either String String
@@ -145,12 +188,15 @@ view state = do
     input ! type' "text" ! value state.username #! onChange UsernameChange
     input ! type' "password" ! value state.password #! onChange PasswordChange
     button #! onClick (const SignIn) $ text "Sign In"
+  
+  div $
+    button #! onClick (const RollDie) $ text "Roll Die"
 
 init :: State
 init = { username : "", password : "" }
 
 -- | Start and render the app
-main :: Eff (CoreEffects (ajax :: AJAX, console :: CONSOLE)) Unit
+main :: Eff (CoreEffects (ajax :: AJAX, console :: CONSOLE, cookie :: COOKIE)) Unit
 main = do
   app <- start
     { initialState: init
